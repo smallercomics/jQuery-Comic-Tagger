864348f0263d6345592b45cf5cded81e1a346a7b
andrewfulton <andrew.fulton@au.penguingroup.com>
2013-06-18 12:01:22 +1000

    #1 Adding extra classes and a 'is tagged' state

--- jquery-comictagger.js
+++ jquery-comictagger.js
@@ -11,6 +11,7 @@
       class_button:"comictag_button",
       separator:" | ",
       class_tag:"add_tag",        //equivalent of imgTag
+      class_tagged:"is_tagged",
       class_clearoff:"clear_off", //imgClearOff
       class_gotooff:"goto_off",   //imgGotoOff
       class_clearon:"clear_on",   //imgClearOn
@@ -18,6 +19,7 @@
       class_info:"info",          //imgInfo
       comic_dir:"/",              //comicDir
       text_tag:"Tag this comic",
+      text_tagged:"This comic is tagged",
       text_goto:"Goto tag",
       text_clear:"Clear tag",
       text_info:"Info?",
@@ -55,13 +57,15 @@
     
     var do_tag = function(o){
       createCookie("bm", options.location, options.cl);
-      $(o).parent().find(".ct_goto").removeClass(options.class_gotooff).addClass(options.class_gotoon);
-      $(o).parent().find(".ct_clear").removeClass(options.class_clearoff).addClass(options.class_clearon);
+      _ct.addClass('ct_is_tagged');
+      _ct.find(".ct_goto").removeClass(options.class_gotooff).addClass(options.class_gotoon);
+      _ct.find(".ct_clear").removeClass(options.class_clearoff).addClass(options.class_clearon);
     }
     var do_clear = function(o){
       createCookie("bm","",-1);
-      $(o).parent().find(".ct_goto").removeClass(options.class_gotoon).addClass(options.class_gotooff);
-      $(o).parent().find(".ct_clear").removeClass(options.class_clearon).addClass(options.class_clearoff);
+      _ct.removeClass('ct_is_tagged');
+      _ct.find(".ct_goto").removeClass(options.class_gotoon).addClass(options.class_gotooff);
+      _ct.find(".ct_clear").removeClass(options.class_clearon).addClass(options.class_clearoff);
     }
     var do_goto = function(){
       var g = readCookie('bm');
@@ -70,12 +74,16 @@
       }	
     }
     var do_info = function(o){
-      $(o).parent().find(".ct_pop").show();
+      _ct.find(".ct_pop").show();
     }
     var hide_info = function(o){
-      $(o).parent().find(".ct_pop").hide();
+      _ct.find(".ct_pop").hide();
     }
+
+    var _ct;
+
     return this.each(function() {
+
       createCookie('t', 1);
       var c = readCookie('t');
       if (c){
@@ -88,22 +96,30 @@
           _goto = options.class_gotooff;
           _clear = options.class_clearoff;
         }
-        obj = $(this);
-        var buttons = "<span class='"+options.class_tag+"'>"+options.text_tag+"</span>" + options.separator
+        _ct = $(this);
+        var buttons = "<span class='ct_tag "+options.class_tag+"'>"+options.text_tag+"</span>"
+                    + "<span class='ct_tagged " + options.class_tagged + "'>" + options.text_tagged + "</span>" + options.separator
                     + "<span class='ct_goto "+_goto+"'>"+options.text_goto+"</span>" + options.separator
                     + "<span class='ct_clear "+_clear+"'>"+options.text_clear+"</span>" + options.separator
                     + "<span class='"+options.class_info+"'>"+options.text_info+"</span>";
         var info_pop = "<div class='ct_pop'>"+ options.text_info_pop+"</div>";
-        obj.html(buttons + info_pop);
-        obj.addClass('_comictagger');
-        // add actions
-        obj.find('.' + options.class_tag).click(function(){
+
+        _ct
+          .html(buttons + info_pop)
+          .addClass('_comictagger');
+
+        if (l==options.location){
+          _ct.addClass('ct_is_tagged');
+        }
+
+
+        _ct.find('.' + options.class_tag).click(function(){
           do_tag( this );
         });
-        obj.find('.' + _goto).click(function(){ do_goto() });
-        obj.find('.' + _clear).click(function(){ do_clear(this) });
-        obj.find('.' + options.class_info).hover(function(){ do_info(this)},function(){ hide_info(this)});
-        obj.find('.ct_pop').hide();
+        _ct.find('.' + _goto).click(function(){ do_goto() });
+        _ct.find('.' + _clear).click(function(){ do_clear(this) });
+        _ct.find('.' + options.class_info).hover(function(){ do_info(this)},function(){ hide_info(this)});
+        _ct.find('.ct_pop').hide();
       }
       
     });
